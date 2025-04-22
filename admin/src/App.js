e3de78f5c967288b2403c75df9d6b83d04ee07b7
NuwangiSubasingha <nuwangisandeepanisubasinghe@gmail.com>
2025-04-22 22:48:28 +0530

    update: added admin frontend section

---
+++ admin/src/App.js
@@ -0,0 +1,22 @@
+import "./app.styles.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Header from "./component/Header/Header";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+const App = () => {
+return <div>
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </Router>
+ </div>;
+};
+
+export default App;
